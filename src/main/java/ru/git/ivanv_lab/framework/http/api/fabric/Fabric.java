dd80e221300985ad0fcae63740b5d54db4bb733c
ivanv <ivanvozisov33@yandex.ru>
2026-08-27 00:23:21 +0300

    Начал пытаться сделать общий Fabric-generic

---
+++ src/main/java/ru/git/ivanv_lab/framework/http/api/fabric/Fabric.java
@@ -0,0 +1,10 @@
+package ru.git.ivanv_lab.framework.http.api.fabric;
+
+import ru.git.ivanv_lab.framework.json.contract_validator.core.JsonContractWorker;
+
+public class Fabric <ALL, INFO> {
+
+    private final JsonContractWorker<ALL> allJsonWorker=
+            new JsonContractWorker<>();
+
+}
