172222900b38185efecc7fff587d52eae5813a31
ivanv <ivanvozisov33@yandex.ru>
2026-08-26 23:57:19 +0300

    Реализовал ретраи запросов с переполучением токена с помощью одного общего метода

--- src/main/java/ru/git/ivanv_lab/framework/http/api/ApiWorker.java
+++ src/main/java/ru/git/ivanv_lab/framework/http/api/ApiWorker.java
@@ -10,6 +10,8 @@ import ru.git.ivanv_lab.framework.http.api.authenticator.BasicApiAuthenticator;
 import ru.git.ivanv_lab.framework.http.api.authenticator.BearerApiAuthenticator;
 import ru.git.ivanv_lab.framework.http.api.authenticator.IApiAuthenticator;
 
+import java.util.function.Supplier;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -38,7 +40,7 @@ public class ApiWorker {
     public ApiWorker get(String url) {
         try {
             log.info("GET {}{}", baseUrl, url);
-            Response resp = given()
+            Supplier<Response> requestSupplier = () -> given()
                     .filter(new AllureRestAssured())
                     .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
                     .contentType(ContentType.JSON)
@@ -46,9 +48,9 @@ public class ApiWorker {
                     .then()
                     .extract().response();
 
-            if (resp.getStatusCode() == 401) {
-                resp = getWithRetry(url);
-            }
+            Response resp = requestSupplier.get();
+            if (resp.getStatusCode() == 401)
+                resp = executeWithRetry(requestSupplier);
 
             response.set(resp);
         } catch (RuntimeException e) {
@@ -58,25 +60,10 @@ public class ApiWorker {
         return this;
     }
 
-    private Response getWithRetry(String url) {
-        for (int attempt = 1; attempt <= 3; attempt++){
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .contentType(ContentType.JSON)
-                    .get(baseUrl + url)
-                    .then()
-                    .extract().response();
-            
-            if(resp.getStatusCode()!=401) return resp;
-        }
-        throw new RuntimeException("Ошибка авторизации");
-    }
-
     public ApiWorker put(String url, String body) {
         try {
-            log.info("PUT {}{}\n{}",baseUrl,url,body);
-            Response resp = given()
+            log.info("PUT {}{}\n{}", baseUrl, url, body);
+            Supplier<Response> requestSupplier = () -> given()
                     .filter(new AllureRestAssured())
                     .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
                     .contentType("application/json")
@@ -86,40 +73,22 @@ public class ApiWorker {
                     .put(baseUrl + url)
                     .then()
                     .extract().response();
-            resp.body().print();
+
+            Response resp = requestSupplier.get();
             if (resp.getStatusCode() == 401)
-                resp = putWithRetry(url, body);
+                resp = executeWithRetry(requestSupplier);
 
             response.set(resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return this;
-    }
-
-    private Response putWithRetry(String url, String body) {
-        for (int attempt = 1; attempt <= 3; attempt++) {
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .contentType("application/json")
-                    .and()
-                    .body(body)
-                    .when()
-                    .put(baseUrl + url)
-                    .then()
-                    .extract().response();
-
-            if (resp.getStatusCode() != 401)
-                return resp;
-        }
-        throw new RuntimeException("Ошибка авторизации");
     }
 
     public ApiWorker post(String url, String body) {
         try {
-            log.info("POST {}{}\n{}",baseUrl,url,body);
-            Response resp = given()
+            log.info("POST {}{}\n{}", baseUrl, url, body);
+            Supplier<Response> requestSupplier = () -> given()
                     .filter(new AllureRestAssured())
                     .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
                     .contentType("application/json;charset=utf-8;")
@@ -130,40 +99,21 @@ public class ApiWorker {
                     .then()
                     .extract().response();
 
-            resp.body().print();
-            if (resp.getStatusCode() == 401) {
-                resp = postWithRetry(url, body);
-            }
+            Response resp = requestSupplier.get();
+            if (resp.getStatusCode() == 401)
+                resp = executeWithRetry(requestSupplier);
 
             response.set(resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return this;
-    }
-
-    private Response postWithRetry(String url, String body) {
-        for (int attempt = 1; attempt <= 3; attempt++) {
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .contentType("application/json;charset=utf-8;")
-                    .and()
-                    .body(body)
-                    .when()
-                    .post(baseUrl + url)
-                    .then()
-                    .extract().response();
-            if (resp.getStatusCode() != 401)
-                return resp;
-        }
-        throw new RuntimeException("Ошибка авторизации");
     }
 
     public ApiWorker patch(String url, String body) {
         try {
-            log.info("PATCH {}{}\n{}",baseUrl,url,body);
-            Response resp = given()
+            log.info("PATCH {}{}\n{}", baseUrl, url, body);
+            Supplier<Response> requestSupplier = () -> given()
                     .filter(new AllureRestAssured())
                     .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
                     .contentType("application/json")
@@ -173,51 +123,10 @@ public class ApiWorker {
                     .patch(baseUrl + url)
                     .then()
                     .extract().response();
-            resp.body().print();
+
+            Response resp = requestSupplier.get();
             if (resp.getStatusCode() == 401)
-                resp = patchWithRetry(url, body);
-            response.set(resp);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return this;
-    }
-
-    private Response patchWithRetry(String url, String body) {
-        for (int attempt = 1; attempt <= 3; attempt++) {
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .contentType("application/json")
-                    .and()
-                    .body(body)
-                    .when()
-                    .patch(baseUrl + url)
-                    .then()
-                    .extract().response();
-
-            if (resp.getStatusCode() != 401)
-                return resp;
-        }
-        throw new RuntimeException("Ошибка авторизации");
-    }
-
-    public ApiWorker delete(String url, String body) {
-        try {
-            log.info("DELETE {}{}\n{}",baseUrl,url,body);
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
-                    .contentType("application/json")
-                    .and()
-                    .body(body)
-                    .when()
-                    .delete(baseUrl + url)
-                    .then()
-                    .extract().response();
-            resp.body().print();
-            if (resp.getStatusCode() == 401)
-                resp = deleteWithRetry(url, body);
+                resp = executeWithRetry(requestSupplier);
 
             response.set(resp);
         } catch (Exception e) {
@@ -228,17 +137,18 @@ public class ApiWorker {
 
     public ApiWorker delete(String url) {
         try {
-            log.info("DELETE {}{}",baseUrl,url);
-            Response resp = given()
+            log.info("DELETE {}{}", baseUrl, url);
+            Supplier<Response> requestSupplier = () -> given()
                     .filter(new AllureRestAssured())
                     .header("Authorization", authenticator.getAuthorization() + authenticator.getToken(contract.get()))
                     .when()
                     .delete(baseUrl + url)
                     .then()
                     .extract().response();
-            resp.body().print();
+
+            Response resp = requestSupplier.get();
             if (resp.getStatusCode() == 401)
-                resp = deleteWithRetry(url);
+                resp = executeWithRetry(requestSupplier);
 
             response.set(resp);
         } catch (Exception e) {
@@ -247,43 +157,21 @@ public class ApiWorker {
         return this;
     }
 
-    private Response deleteWithRetry(String url) {
+    private Response executeWithRetry(Supplier<Response> requestSupplier) {
         for (int attempt = 1; attempt <= 3; attempt++) {
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .when()
-                    .delete(baseUrl + url)
-                    .then()
-                    .extract().response();
+            authenticator.resetToken(contract.get());
+            Response resp = requestSupplier.get();
+
             if (resp.getStatusCode() != 401)
                 return resp;
         }
         throw new RuntimeException("Ошибка авторизации");
     }
 
-    private Response deleteWithRetry(String url, String body) {
-        for (int attempt = 1; attempt <= 3; attempt++) {
-            Response resp = given()
-                    .filter(new AllureRestAssured())
-                    .header("Authorization", authenticator.getAuthorization() + authenticator.resetToken(contract.get()))
-                    .contentType("application/json")
-                    .and()
-                    .body(body)
-                    .when()
-                    .delete(baseUrl + url)
-                    .then()
-                    .extract().response();
-            if (resp.getStatusCode() != 401)
-                return resp;
-        }
-        throw new RuntimeException("Ошибка авторизации");
-    }
-
-    public ApiWorker code(int code){
-        try{
+    public ApiWorker code(int code) {
+        try {
             assertEquals(code, response.get().getStatusCode());
-        } catch (AssertionError e){
+        } catch (AssertionError e) {
             throw new RuntimeException("""
                     Ожидаемый статус: %d
                     Фактический статус: %d
@@ -292,7 +180,7 @@ public class ApiWorker {
         return this;
     }
 
-    public Response getResponse(){
+    public Response getResponse() {
         return response.get();
     }
 }
